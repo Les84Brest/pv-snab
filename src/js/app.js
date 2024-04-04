6e9451887950c0dac77fdf8ef8eb5bebf02355f8
Aliaksei Semianiuk <seman17061984@gmail.com>
2024-04-04 09:57:41 +0300

    contactsSection: modals added and stylized

--- src/js/app.js
+++ src/js/app.js
@@ -15,19 +15,22 @@ document.addEventListener('DOMContentLoaded', function () {
     //init modal for company data
     const modalOptions = {
         templateId: "company-data", // template tag with modal layout
+        title: "Наши реквизиты",
         selectors: {
             modal: 'modal',
             modalBody: 'modal__body',
             modalClose: 'modal__close',
             modalOpen: 'modal__open',
-            bodyLock: '_scroll-lock'
+            bodyLock: '_scroll-lock',
+            modalTitle: 'modal__title',
+            modalContent: 'modal__content'
         }
     }
     const companyDataModal = new Modal(modalOptions);
     const dataBtn = document.querySelector('.contacts__action-btn');
     dataBtn.addEventListener("click", function (e) {
         companyDataModal.openModal();
-        
+
     });
 });
 
--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -11,31 +11,35 @@ class Modal {
 
   options = {
     templateId: null, // template tag with modal layout
+    title: null,
     selectors: {
       modal: 'modal',
       modalBody: 'modal__body',
       modalClose: 'modal__close',
       modalOpen: 'modal__open',
-      bodyLock: 'lock-body'
+      bodyLock: 'lock-body',
+      modalTitle: 'modal__title',
+      modalContent: 'modal__content',
     }
   }
 
-  constructor(options) {
 
+  constructor(options) {
+    console.log('%coptions', 'padding: 5px; background: hotpink; color: black;', this.options.selectors);
     this.options = { ...this.options, ...options };
     this.initModal();
   }
 
   initModal() {
     const { modal: modalClass, modalClose, modalBody } = this.options.selectors;
-
     const modalNode = document.querySelector(`.${modalClass}`);
+
 
     if (modalNode) {
       this.modalObject = modalNode;
+      this.resetModal();
 
       this.modalObject.addEventListener('click', (event) => {
-        console.log('%clistener works', 'padding: 5px; background: #3dd; color: #333333;');
         if (!event.target.closest(`.${modalBody}`)) {
           this.closeModal();
         }
@@ -45,10 +49,45 @@ class Modal {
         .addEventListener('click', this.closeModal.bind(this));
 
     }
+
+
   }
 
+  setModalTitle() {
+    const { title } = this.options;
+    const { modalTitle } = this.options.selectors;
+
+    if (!title) {
+      return;
+    }
+
+    const titleNode = this.modalObject.querySelector(`.${modalTitle}`);
+
+    if (modalTitle) {
+      titleNode.innerText = title;
+    }
+
+  }
+
+  setModalContent() {
+    const { templateId } = this.options;
+    const { modalContent } = this.options.selectors;
+
+    const template = document.querySelector(`#${templateId}`);
+    const clonedContent = template.content.cloneNode(true);
+    const content = this.modalObject.querySelector(`.${modalContent}`);
+
+    // content.insertAdjacentHTML('beforeend', clonedContent);
+    content.append(clonedContent);
+    // content.innerHTML = clonedContent;
+  }
   openModal() {
     const { modalOpen } = this.options.selectors;
+    this.resetModal();
+
+    this.setModalTitle();
+    this.setModalContent();
+
     this.modalObject.classList.add(`${modalOpen}`);
     this.lockBody();
   }
@@ -70,6 +109,13 @@ class Modal {
     const { bodyLock } = this.options.selectors;
     document.body.style.paddingRight = null;
     document.body.classList.remove(`${bodyLock}`);
+  }
+
+  resetModal() {
+    const { modalContent, modalTitle } = this.options.selectors;
+    this.modalObject.querySelector(`.${modalContent}`).innerText = '';
+    this.modalObject.querySelector(`.${modalTitle}`).innerText = '';
+
   }
 }
 
